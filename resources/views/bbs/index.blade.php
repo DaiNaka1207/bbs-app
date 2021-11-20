c6446c698cd58b9c27e058ea6663c6d3011c8ece
DaiNaka1207 <77893229+DaiNaka1207@users.noreply.github.com>
2021-11-20 19:33:11 +0900

    Add BBS_view & routing

---
+++ resources/views/bbs/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>{{ env('app_name') }}</title>
+    
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
+</head>
+<body>
+    <p class="text-blue-500">^^)v Hello World</p>
+</body>
+</html>
